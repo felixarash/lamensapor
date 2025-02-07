eb61c72cce4bc0bb6d4ce283dba1ab246ce153ef
felixarash <fozanahmedm9@gmail.com>
2025-02-07 12:02:39 +0500

    sync: local changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,8 @@ import { Inter } from 'next/font/google'
 import Header from '@/components/layout/Header'
 import Footer from '@/components/layout/Footer'
 import CartSidebar from '@/components/cart/CartSidebar'
-import { Providers } from '@/components/providers/Providers'
+import { CartProvider } from '@/contexts/CartContext'
+import { Toaster } from 'react-hot-toast'
 import '@/app/globals.css'
 
 const inter = Inter({ 
@@ -23,14 +24,15 @@ export default function RootLayout({
   return (
     <html lang="en" className="scroll-smooth">
       <body className={`${inter.variable} font-sans antialiased`}>
-        <Providers>
+        <CartProvider>
           <div className="flex min-h-screen flex-col">
             <Header />
             <main className="flex-1">{children}</main>
             <CartSidebar />
             <Footer />
           </div>
-        </Providers>
+          <Toaster position="top-right" />
+        </CartProvider>
       </body>
     </html>
   )
