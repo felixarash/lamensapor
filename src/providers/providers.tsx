e61778f5567d36573e8375f853872ed623e61882
felixarash <fozanahmedm9@gmail.com>
2025-02-07 12:58:57 +0500

    {commit_message}

--- src/providers/providers.tsx
+++ src/providers/providers.tsx
@@ -1,16 +1,22 @@
 'use client'
 
-import { SessionProvider } from 'next-auth/react'
 import { CartProvider } from '@/contexts/CartContext'
 import { Toaster } from 'react-hot-toast'
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <SessionProvider>
-      <CartProvider>
-        {children}
-        <Toaster position="top-right" />
-      </CartProvider>
-    </SessionProvider>
+    <CartProvider>
+      {children}
+      <Toaster 
+        position="top-right"
+        toastOptions={{
+          duration: 3000,
+          style: {
+            background: '#333',
+            color: '#fff',
+          }
+        }}
+      />
+    </CartProvider>
   )
 }
