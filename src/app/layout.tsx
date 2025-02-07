044f1da05424103ba00184b24de54674c02a993d
felixarash <fozanahmedm9@gmail.com>
2025-02-07 11:58:14 +0500

    sync: local changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,9 @@
 import { Inter } from 'next/font/google'
-import { CartProvider } from '@/contexts/CartContext'
 import Header from '@/components/layout/Header'
 import Footer from '@/components/layout/Footer'
 import CartSidebar from '@/components/cart/CartSidebar'
 import { Providers } from '@/components/providers/Providers'
 import '@/app/globals.css'
-import { Toaster } from 'react-hot-toast'
 
 const inter = Inter({ 
   subsets: ['latin'],
@@ -28,12 +26,9 @@ export default function RootLayout({
         <Providers>
           <div className="flex min-h-screen flex-col">
             <Header />
-            <CartProvider>
-              <main className="flex-1">{children}</main>
-              <CartSidebar />
-              <Footer />
-              <Toaster position="top-right" />
-            </CartProvider>
+            <main className="flex-1">{children}</main>
+            <CartSidebar />
+            <Footer />
           </div>
         </Providers>
       </body>
